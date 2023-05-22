bfc68193b453767756987b6a9e66e6c23ebc8d0b
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-22 14:39:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,4 +12,39 @@ const apiUrls = [
   "https://jsonplaceholder.typicode.com/todos/10",
 ];
 
+var allTimer = 0;
+var anyTimer = 0;
+
+const outputAll = document.querySelector("#output-all");
+const outputAny = document.querySelector("#output-any");
+
+const all = async function () {
+  while (true) {
+    allTimer++;
+    const promise = await Promise.all(
+      apiUrls.map((val) => {
+        return fetch(val);
+      })
+    );
+    outputAll.textContent = allTimer;
+    return promise;
+  }
+};
+
+const any = async function () {
+  while (true) {
+    anyTimer++;
+    const promise = await Promise.any(
+      apiUrls.map((val) => {
+        return fetch(val);
+      })
+    );
+    outputAny.innerText = anyTimer;
+    return promise;
+  }
+};
+
+all();
+any();
+
 // You can write your code here
